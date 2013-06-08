0249dcd3a1ef6156f6de665213de9bb32d3167e0
Slobodan Manic <slobodan.manic@gmail.com>
2013-06-08 13:02:47 +0100

    Latest

--- footer.php
+++ footer.php
@@ -8,18 +8,21 @@
  */
 ?>
 
+		</div><!-- .inner -->
 	</div><!-- #main -->
 
 	<?php tha_footer_before(); ?>
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<?php tha_footer_top(); ?>
-		<div class="site-info">
-			<?php do_action( 'gumbo_credits' ); ?>
-			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'gumbo' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'gumbo' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'gumbo' ), 'Gumbo', '<a href="http://thematosoup.com" rel="designer">ThematoSoup</a>' ); ?>
-		</div><!-- .site-info -->
-		<?php tha_footer_bottom(); ?>
+		<div class="inner">
+			<?php tha_footer_top(); ?>
+			<div class="site-info">
+				<?php do_action( 'gumbo_credits' ); ?>
+				<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'gumbo' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'gumbo' ), 'WordPress' ); ?></a>
+				<span class="sep"> | </span>
+				<?php printf( __( 'Theme: %1$s by %2$s.', 'gumbo' ), 'Gumbo', '<a href="http://thematosoup.com" rel="designer">ThematoSoup</a>' ); ?>
+			</div><!-- .site-info -->
+			<?php tha_footer_bottom(); ?>
+		</div><!-- .inner -->
 	</footer><!-- #colophon -->
 	<?php tha_footer_after(); ?>
 </div><!-- #page -->
