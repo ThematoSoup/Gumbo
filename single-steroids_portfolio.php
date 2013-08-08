0e910f2dc8bd8766e814e7302684388c84b8788b
Slobodan Manic <slobodan.manic@gmail.com>
2013-08-08 12:54:54 +0100

    Added single Steroids project template

---
+++ single-steroids_portfolio.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The Template for displaying single portfolio project.
+ * Requires Steroids plugin and its Portfolio module to be active
+ *
+ * @package Gumbo
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<?php tha_content_before(); ?>
+		<div id="content" class="site-content" role="main">
+			<?php tha_content_top(); ?>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+	
+				<?php get_template_part( '/partials/content', 'single' ); ?>
+	
+				<?php thsp_content_nav( 'nav-below' ); ?>
+	
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template
+					if ( comments_open() || '0' != get_comments_number() )
+						comments_template();
+				?>
+	
+			<?php endwhile; // end of the loop. ?>
+
+			<?php tha_content_bottom(); ?>
+		</div><!-- #content -->
+		<?php tha_content_after(); ?>
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
