d4175b286407a1f6538e07bc7951311cdc394c23
Slobodan Manic <slobodan.manic@gmail.com>
2014-01-15 13:07:10 +0100

    Inline docu

--- sidebar.php
+++ sidebar.php
@@ -2,13 +2,17 @@
 /**
  * The Sidebar containing the main widget area.
  *
+ * For single posts and pages custom widget area is stored
+ * as a custom field. If WooSidebars plugin is active, this
+ * custom widget area is used instead of Primary Sidebar.
+ *
  * @package		Gumbo
  * @since		Gumbo 1.0
  */
 ?>
 
 <?php
-$current_sidebar = ( get_post_meta( $post->ID, '_thsp_widget_area', true ) ? get_post_meta( $post->ID, '_thsp_widget_area', true ) : 'primary-sidebar' );
+$current_sidebar = ( in_array( 'woosidebars/woosidebars.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) && get_post_meta( $post->ID, '_thsp_widget_area', true ) ? get_post_meta( $post->ID, '_thsp_widget_area', true ) : 'primary-sidebar' );
 if ( is_active_sidebar( $current_sidebar ) && 'no-sidebar' != thsp_get_current_layout() ) : // Only render this sidebar in not in single column layout ?>
 	<?php tha_sidebars_before(); ?>
 	<div id="secondary" class="widget-area" role="complementary">
