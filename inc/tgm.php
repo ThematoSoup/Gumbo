170d0ee1562c92b22c38f5d86418c74ba27ef904
Slobodan Manic <slobodan.manic@gmail.com>
2014-05-21 09:06:28 +0200

    Updated Genericons font to 3.0.4

--- inc/tgm.php
+++ inc/tgm.php
@@ -51,9 +51,6 @@ function gumbo_register_required_plugins() {
 		
 	);
 	
-	// Change this to your theme text domain, used for internationalising strings
-	$theme_text_domain = 'gumbo';
-	
 	/**
 	* Array of configuration settings. Amend each line as needed.
 	* If you want the default strings to be available under your own theme domain,
@@ -62,7 +59,7 @@ function gumbo_register_required_plugins() {
 	* end of each line for what each argument will be.
 	*/
 	$config = array(
-		'domain'            => $theme_text_domain,           // Text domain - likely want to be the same as your theme.
+		'domain'            => 'gumbo',           // Text domain - likely want to be the same as your theme.
 		'default_path'      => '',                           // Default absolute path to pre-packaged plugins
 		'parent_menu_slug'  => 'themes.php',         // Default parent menu slug
 		'parent_url_slug'   => 'themes.php',         // Default parent URL slug
@@ -71,10 +68,10 @@ function gumbo_register_required_plugins() {
 		'is_automatic'      => false,            // Automatically activate plugins after installation or not
 		'message'           => '',               // Message to output right before the plugins table
 		'strings'           => array(
-			'page_title'                                => __( 'Install Required Plugins', $theme_text_domain ),
-			'menu_title'                                => __( 'Install Plugins', $theme_text_domain ),
-			'installing'                                => __( 'Installing Plugin: %s', $theme_text_domain ), // %1$s = plugin name
-			'oops'                                      => __( 'Something went wrong with the plugin API.', $theme_text_domain ),
+			'page_title'                                => __( 'Install Required Plugins', 'gumbo' ),
+			'menu_title'                                => __( 'Install Plugins', 'gumbo' ),
+			'installing'                                => __( 'Installing Plugin: %s', 'gumbo' ), // %1$s = plugin name
+			'oops'                                      => __( 'Something went wrong with the plugin API.', 'gumbo' ),
 			'notice_can_install_required'               => _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.' ), // %1$s = plugin name(s)
 			'notice_can_install_recommended'            => _n_noop( 'This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.' ), // %1$s = plugin name(s)
 			'notice_cannot_install'                     => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.' ), // %1$s = plugin name(s)
@@ -85,9 +82,9 @@ function gumbo_register_required_plugins() {
 			'notice_cannot_update'                      => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.' ), // %1$s = plugin name(s)
 			'install_link'                              => _n_noop( 'Begin installing plugin', 'Begin installing plugins' ),
 			'activate_link'                             => _n_noop( 'Activate installed plugin', 'Activate installed plugins' ),
-			'return'                                    => __( 'Return to Required Plugins Installer', $theme_text_domain ),
-			'plugin_activated'                          => __( 'Plugin activated successfully.', $theme_text_domain ),
-			'complete'                                  => __( 'All plugins installed and activated successfully. %s', $theme_text_domain ) // %1$s = dashboard link
+			'return'                                    => __( 'Return to Required Plugins Installer', 'gumbo' ),
+			'plugin_activated'                          => __( 'Plugin activated successfully.', 'gumbo' ),
+			'complete'                                  => __( 'All plugins installed and activated successfully. %s', 'gumbo' ) // %1$s = dashboard link
 		)
 	);
 	
